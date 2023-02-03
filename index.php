b27e7deb1a0955e2877b103fac5180e9ed06ec34
GiadaGallitto <gallitto.giada@gmail.com>
2023-02-03 15:30:04 +0100

    Add Class and construct function

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    class Movie {
+        public $name;
+        public $genre;
+        public $vote;
+
+        function __construct($_name, $_genre, $_vote) {
+            $this->name = $_name;
+            $this->genre = $_genre;
+            $this->vote = $_vote;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Op Movies</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
